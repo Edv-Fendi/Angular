e978bf93e72d671658df071088a622aee04b78f9
Edvaldo Fendi <edvaldo.defendi@gmail.com>
2022-11-18 13:05:15 -0300

    injetando dependencias

--- hola_controller/controller.js
+++ hola_controller/controller.js
@@ -1,19 +1,19 @@
 var app = angular.module("MyFirtApp", []);
-app.controller("FirstController", function ($scope) {
-    $scope.nombre = "Edvaldo";
-    $scope.nuevoComentario = {};
-    $scope.comentarios = [
-        {
-            comentario: "Salve",
-            username: "Jorge"
-        },
-        {
-            comentario: "Pessimo exemplo",
-            username: "Edv"
-        }
+app.controller("FirstController", ["$scope", "$http", function (m, $http) {
+    m.nombre = "Edvaldo";
+    m.nuevoComentario = {};
+    m.comentarios = [
+        // {
+        //     comentario: "Salve",
+        //     username: "Jorge"
+        // },
+        // {
+        //     comentario: "Pessimo exemplo",
+        //     username: "Edv"
+        // }
     ];
-    $scope.agregarComentario = function(){
-        $scope.comentarios.push($scope.nuevoComentario);
-        $scope.nuevoComentario = {};
+    m.agregarComentario = function(){
+        m.comentarios.push(m.nuevoComentario);
+        m.nuevoComentario = {};
     }
-});
+}]);
