ab4fe60f5ac84fcf5b23dc9a835da48017a45479
Edvaldo Fendi <edvaldo.defendi@gmail.com>
2022-11-18 14:39:17 -0300

    post

--- http/controller.js
+++ http/controller.js
@@ -1,13 +1,32 @@
 angular.module("MyFirstApp", [])
-.controller("FirstController", function($scope, $http){
+.controller("FirstController", ['$scope', '$http', function($scope, $http){
     $scope.posts = [];
+    $scope. newPost = {};
     $http.get("http://jsonplaceholder.typicode.com/posts")
     .then(function(data){
             console.log(data);
-            $scope.posts = data;
+            $scope.posts = data.data;
 
         })
-        .error(function(err){
+        .catch(function(err){
 
         });
-});
+
+        $scope.addPost = function(){
+            $http.post("http://jsonplaceholder.typicode.com/posts", {
+                title: $scope.newPost.title,
+                body: $scope.newPost.body,
+                userId: 1
+            })
+            .then(function(data, status, headers, config){ 
+                console.log(data);
+                $scope.posts.push($scope.newPost)
+                $scope.addPost = {};
+               
+
+            })
+            .catch(function(error, status, headers, config){
+                console.log(error);
+            })
+        }
+}]);
