09286d7bd2ad398f22262284b4e1bbcdcd2d05af
Edvaldo Fendi <edvaldo.defendi@gmail.com>
2022-11-18 12:06:09 -0300

    comentarios

--- hola_controller/controller.js
+++ hola_controller/controller.js
@@ -1,4 +1,19 @@
 var app = angular.module("MyFirtApp", []);
-app.controller("FirstController", function($scope){
+app.controller("FirstController", function ($scope) {
     $scope.nombre = "Edvaldo";
+    $scope.nuevoComentario = {};
+    $scope.comentarios = [
+        {
+            comentario: "Salve",
+            username: "Jorge"
+        },
+        {
+            comentario: "Pessimo exemplo",
+            username: "Edv"
+        }
+    ];
+    $scope.agregarComentario = function(){
+        $scope.comentarios.push($scope.nuevoComentario);
+        $scope.nuevoComentario = {};
+    }
 });
